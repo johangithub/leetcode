404250fc48e43a31ddba3fc3aa59e0d0bd8b1992
johangithub <joe.han0212@gmail.com>
2018-02-20 13:00:54 -0600

    Create 111.py

---
+++ 111.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        d={}
+        if not root:
+            return 0
+        levels = []
+        def dfs(node,level):
+            if level in d:
+                d[level].append(node.val)
+            else:
+                d[level] = [node.val]
+            
+            if node.left:
+                dfs(node.left, level+1)
+            if node.right:
+                dfs(node.right, level+1)
+            if not (node.left or node.right):
+                levels.append(level)
+            
+        dfs(root, 1)
+        return min(levels)
